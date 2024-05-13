d89facc60e90e9ee6a0f827ab7ef56b6ed9c80d6
Oleksii Bosko <92023689+Cruci13@users.noreply.github.com>
2024-05-13 23:13:55 +0300

    Add files via upload

---
+++ PP_lab9/PP_lab9/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+
+abstract class Component
+{
+    public abstract void Operation();
+}
+
+
+class ConcreteComponent : Component
+{
+    public override void Operation()
+    {
+        Console.WriteLine("ConcreteComponent.Operation()");
+    }
+}
+
+
+abstract class Decorator : Component
+{
+    protected Component component;
+
+    public Decorator(Component component)
+    {
+        this.component = component;
+    }
+
+    public override void Operation()
+    {
+        if (component != null)
+        {
+            component.Operation();
+        }
+    }
+}
+
+
+class ConcreteDecoratorA : Decorator
+{
+    public ConcreteDecoratorA(Component component) : base(component)
+    {
+    }
+
+    public override void Operation()
+    {
+        base.Operation();
+        Console.WriteLine("ConcreteDecoratorA.Operation()");
+    }
+}
+
+
+class ConcreteDecoratorB : Decorator
+{
+    public ConcreteDecoratorB(Component component) : base(component)
+    {
+    }
+
+    public override void Operation()
+    {
+        base.Operation();
+        AddedBehavior();
+        Console.WriteLine("ConcreteDecoratorB.Operation()");
+    }
+
+    void AddedBehavior()
+    {
+        Console.WriteLine("ConcreteDecoratorB.AddedBehavior()");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Component component = new ConcreteComponent();
+
+        
+        Decorator decoratorA = new ConcreteDecoratorA(component);
+        Decorator decoratorB = new ConcreteDecoratorB(decoratorA);
+
+        
+        decoratorB.Operation();
+    }
+}
